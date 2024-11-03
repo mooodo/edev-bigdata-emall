2c82fdc3f956c08dc8f43e46d1066e4fcc5981f6
Fangang <fangang2004@126.com>
2024-11-03 23:32:34 +0800

    fix a bug

--- emall-bigdata-spark/src/main/scala/com/edev/emall/hudi/dw/dim/DwDimCategoryHierarchy.scala
+++ emall-bigdata-spark/src/main/scala/com/edev/emall/hudi/dw/dim/DwDimCategoryHierarchy.scala
@@ -7,11 +7,11 @@ object DwDimCategoryHierarchy {
   def main(args: Array[String]): Unit = {
     val num = PropertyFile.getProperty("numPartitions").toInt
     val spark = SparkUtils.init("dw_dim_category_hierarchy")
-    val data = spark.sql("select parent_id, parent_name, child_id, child_name, level " +
+    val data = spark.sql("select parent_id, parent_name, child_id, child_name, `level` " +
       "from emall_etl.etl_category_hierarchy").repartition(num)
     HudiUtils.saveAppend(data, "hudi_dw", "dw_dim_category_hierarchy",
       HudiConf.build()
-        .option(RECORDKEY_FIELD.key(), "id")
+        .option(RECORDKEY_FIELD.key(), "parent_id,child_id")
         .option(TABLE_TYPE.key(), "COPY_ON_WRITE"))
   }
 }
